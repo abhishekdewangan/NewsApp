b539f29811950e94a60dffd612d30799c8e0f1b9
Dewangan <abhishekdewangan@theporter.in>
2019-10-25 19:46:11 +0530

    saving selected categories in shared pref

--- shared/src/commonMain/kotlin/news_category/data/NewsCategoryRepoImpl.kt
+++ shared/src/commonMain/kotlin/news_category/data/NewsCategoryRepoImpl.kt
@@ -1,13 +1,27 @@
 package news_category.data
 
+import com.russhwolf.settings.Settings
+import constants.SharedPrefConstants
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.launch
 import news_category.domain.NewsCategory
 import news_category.domain.NewsCategoryRepo
+import kotlin.coroutines.CoroutineContext
 
-class NewsCategoryRepoImpl : NewsCategoryRepo {
-    private val selectedCategoriesSet = HashSet<NewsCategory>()
+class NewsCategoryRepoImpl(
+    private val settings: Settings,
+    override val coroutineContext: CoroutineContext
+) : NewsCategoryRepo, CoroutineScope {
+    private val selectedCategoriesSet: HashSet<NewsCategory> = HashSet()
     private val selectedCategoryChannel = Channel<List<NewsCategory>>()
+
+    init {
+        launch {
+            initialRepoSetup()
+        }
+    }
 
     override fun getAll(): List<NewsCategory> {
         return NewsCategory.values().toList()
@@ -18,17 +32,41 @@ class NewsCategoryRepoImpl : NewsCategoryRepo {
     }
 
     override fun getSelectedCategoriesChannel(): ReceiveChannel<List<NewsCategory>> {
-       return selectedCategoryChannel
+        return selectedCategoryChannel
     }
 
     override suspend fun remove(category: NewsCategory) {
         selectedCategoriesSet.remove(category)
-        selectedCategoryChannel.send(selectedCategoriesSet.toList())
+        updateSelectedCategory()
     }
 
     override suspend fun add(category: NewsCategory) {
         selectedCategoriesSet.add(category)
-        selectedCategoryChannel.send(selectedCategoriesSet.toList())
+        updateSelectedCategory()
     }
 
+    private suspend fun initialRepoSetup() {
+        val selectedNewsCategoryKeys = settings
+            .getString(SharedPrefConstants.KEY_SELECTED_CATEGORY)
+            .split(",")
+            .toSet()
+
+        getAll()
+            .filter { selectedNewsCategoryKeys.contains(it.key) }
+            .apply { selectedCategoriesSet.addAll(this) }
+
+        updateSelectedCategory()
+    }
+
+    private suspend fun updateSelectedCategory() {
+        selectedCategoryChannel.send(selectedCategoriesSet.toList())
+
+        val selectedCategories = if (selectedCategoriesSet.isEmpty()) "" else {
+            selectedCategoriesSet
+                .map { it.key }
+                .reduce { categoryName1, categoryName2 -> "$categoryName1,$categoryName2" }
+        }
+
+        settings.putString(SharedPrefConstants.KEY_SELECTED_CATEGORY, selectedCategories)
+    }
 }
