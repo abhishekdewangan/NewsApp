d9de0f8c50e74490d0d02dd92a11a1589713bdd6
Dewangan <abhishekdewangan@theporter.in>
2019-10-25 12:54:22 +0530

    created common widget

---
+++ app/src/main/java/com/sample/newsapp/widget/RegularButton.kt
@@ -0,0 +1,19 @@
+package com.sample.newsapp.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import butterknife.ButterKnife
+import com.sample.newsapp.R
+
+class RegularButton constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int = 0) :
+    FrameLayout(context, attrs, defStyleAttr) {
+    constructor(context: Context): this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    init {
+        val rootView = inflate(context, R.layout.partial_regular_button, this)
+        ButterKnife.bind(this, rootView)
+    }
+}
