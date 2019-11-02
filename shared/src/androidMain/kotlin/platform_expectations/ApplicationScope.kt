53eee44b499a4bd44cde85a1bdedcdcfe4d00431
Dewangan <abhishekdewangan@theporter.in>
2019-11-02 16:43:31 +0530

    implemented expected dispatchers

--- shared/src/androidMain/kotlin/platform_expectations/ApplicationScope.kt
+++ shared/src/androidMain/kotlin/platform_expectations/ApplicationScope.kt
@@ -3,8 +3,11 @@ package platform_expectations
 import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.android.Android
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
 actual val appCoroutineContext: CoroutineContext = Dispatchers.Main
 actual val httpClientEngineFactory: HttpClientEngineFactory<HttpClientEngineConfig> = Android
+actual val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+actual val mainDispatcher: CoroutineDispatcher = Dispatchers.Main
--- shared/src/commonMain/kotlin/platform_expectations/ApplicationScope.kt
+++ shared/src/commonMain/kotlin/platform_expectations/ApplicationScope.kt
@@ -2,8 +2,13 @@ package platform_expectations
 
 import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlin.coroutines.CoroutineContext
 
 expect val appCoroutineContext: CoroutineContext
 
 expect val httpClientEngineFactory: HttpClientEngineFactory<HttpClientEngineConfig>
+
+expect val ioDispatcher: CoroutineDispatcher
+
+expect val mainDispatcher: CoroutineDispatcher
