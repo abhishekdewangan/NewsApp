72fb8f9f8b7862dcff8682708daa7f88acbe18a2
Dewangan <abhishekdewangan@theporter.in>
2019-11-04 23:43:00 +0530

    setting default http client setup

--- shared/src/commonMain/kotlin/network/NetworkUtils.kt
+++ shared/src/commonMain/kotlin/network/NetworkUtils.kt
@@ -2,12 +2,14 @@ package network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.features.defaultRequest
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.features.logging.DEFAULT
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logger
 import io.ktor.client.features.logging.Logging
+import io.ktor.http.URLProtocol
 
 
 class NetworkUtils(httpClientEngine: HttpClientEngine)  {
@@ -19,6 +21,14 @@ class NetworkUtils(httpClientEngine: HttpClientEngine)  {
 
         install(JsonFeature) {
             serializer = KotlinxSerializer()
+        }
+
+        defaultRequest {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "newsapi.org"
+                parameters.append("apiKey", "3cf3a461818d4c628833f46e9e11ef45")
+            }
         }
     }
 }
