729f5b1e028f1ed26b7c70cb9572a395db508967
Dewangan <abhishekdewangan@theporter.in>
2019-11-05 09:41:47 +0530

    impl todo

--- app/src/main/java/com/sample/newsapp/widget/RegularButton.kt
+++ app/src/main/java/com/sample/newsapp/widget/RegularButton.kt
@@ -3,8 +3,11 @@ package com.sample.newsapp.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatImageView
+import butterknife.BindView
 import butterknife.ButterKnife
 import com.sample.newsapp.R
+
 
 class RegularButton constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int = 0) :
     FrameLayout(context, attrs, defStyleAttr) {
@@ -12,9 +15,19 @@ class RegularButton constructor(context: Context, attrs: AttributeSet?, defStyle
 
   constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
-  //todo add support for changing button text and image
+  @BindView(R.id.img_btn)
+  lateinit var buttonImage:AppCompatImageView
+
+  @BindView(R.id.tv_button_text)
+  lateinit var buttonText: RegularTextView
+
   init {
     val rootView = inflate(context, R.layout.partial_regular_button, this)
     ButterKnife.bind(this, rootView)
+    val styleAttributes = getContext().obtainStyledAttributes(attrs, R.styleable.RegularButton)
+    val text = styleAttributes.getText(R.styleable.RegularButton_text)
+    val image = styleAttributes.getResourceId(R.styleable.RegularButton_image, R.drawable.ic_arrow_forward)
+    buttonImage.setImageResource(image)
+    buttonText.text = text
   }
 }
